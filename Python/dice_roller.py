aea2bfa6a77b0f36324b65ffcaabeceb333eaca7
adent11 <55511021+adent11@users.noreply.github.com>
2021-09-14 11:24:44 -0400

    Update dice_roller.py

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -5,11 +5,17 @@
 from random import randint
 
 print ("Automatic Dice Roller")
-
-numSides = input("How many sides do you want")
+print ("")
+print ("How many sides do you want?")
+numSides = input("")
+print ("")
+print ("How many dice do you want to roll?")
+numRolls = input("")
 
 while True:
+    print("")
     inStr = input("Press enter to roll again, press x to exit: ")
     if inStr == 'x':
         break #stops loop, goes to statement after loop
-    print (randint(1, numSides))
+    for x in range(int(numRolls)):
+        print ("The value is:   " + str(randint(1, int(numSides))))
