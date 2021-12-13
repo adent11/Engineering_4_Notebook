adb660ba9d41abcda747edb5a224d436b585c245
adent11 <adent11@charlottesvilleschools.org>
2021-12-13 17:24:22 +0000

    Delete test files and take new video

--- Python/PIR.py
+++ Python/PIR.py
@@ -5,12 +5,16 @@ from datetime import datetime
 pir = MotionSensor(23)
 camera = PiCamera()
 
+now = datetime.now()
+print(now)
+
 while True:
 	#print(pir.motion_detected)
 	filename = "helpme.h264"
 	pir.wait_for_motion()
 	camera.start_recording(filename)
+	print("Recording Started")
 	pir.wait_for_no_motion()
 	camera.stop_recording()
-	print("Hello")
+	print("Recording Stopped")
 
